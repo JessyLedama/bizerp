cc628601dd143e2560c9b6809c3dae6a068f535c
Jayliste <jaylisteledama94@gmail.com>
2023-09-13 11:07:02 +0300

    [ADD] added sidenav

---
+++ resources/views/layouts/side-nav.blade.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="{{ asset('css/side-nav.css') }}">
+
+<nav>
+    <div class="sidenav">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+    </div> 
+</nav>   
+
+
