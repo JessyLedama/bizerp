a0530b0adb3b3c45a6be313c4faeba7e88db4b61
Jayliste <jaylisteledama94@gmail.com>
2023-09-15 10:34:03 +0300

    [UPDATE] updated list,grid and graph view in users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+<link rel="stylesheet" href="{{ asset('css/users.css') }}">
 
 @section('content')
     <section>
@@ -14,13 +15,19 @@
                     @if(isset($users))
                     <ul class="nav nav-pills mb-3 justify-content-end" id="pills-tab" role="tablist">
                         <li class="nav-item">
-                            <a class="nav-link active" id="pills-list-tab" data-toggle="pill" href="#pills-list" role="tab" aria-controls="pills-list" aria-selected="true">List</a>
+                            <a class="nav-link active" id="pills-list-tab" data-toggle="pill" href="#pills-list" role="tab" aria-controls="pills-list" aria-selected="true">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z"/></svg>
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="pills-grid-tab" data-toggle="pill" href="#pills-grid" role="tab" aria-controls="pills-grid" aria-selected="false">Grid</a>
+                            <a class="nav-link" id="pills-grid-tab" data-toggle="pill" href="#pills-grid" role="tab" aria-controls="pills-grid" aria-selected="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M128 136c0-22.1-17.9-40-40-40L40 96C17.9 96 0 113.9 0 136l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40l0-48zm0 192c0-22.1-17.9-40-40-40H40c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM288 328c0-22.1-17.9-40-40-40H200c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM448 328c0-22.1-17.9-40-40-40H360c-22.1 0-40 17.9-40 40v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328z"/></svg>
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+                            <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M160 80c0-26.5 21.5-48 48-48h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V80zM0 272c0-26.5 21.5-48 48-48H80c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272zM368 96h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H368c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z"/></svg>
+                            </a>
                         </li>
                     </ul>
 
@@ -74,7 +81,135 @@
 
                         <!-- GRID VIEW -->
                         <div class="tab-pane fade" id="pills-grid" role="tabpanel" aria-labelledby="pills-grid-tab">
-                            Grid
+                            
+                            <div class="container">
+                                <div class="row">
+                                    @foreach($users as $user)
+                                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                                        <div class="our-team">
+                                            <div class="picture">
+                                                <img class="img-fluid" src="{{ asset('/storage'.$user->photo) }}">
+                                            </div>
+
+                                            <div class="team-content">
+                                                <h3 class="name">
+                                                    {{ $user->firstName }}
+                                                    {{ $user->firstName }}
+                                                </h3>
+                                                <h4 class="title">
+                                                    {{ $user->phone }} 
+                                                </h4>
+
+                                                <h4 class="title">
+                                                    {{ $user->email }}
+                                                </h4>
+                                            </div>
+
+                                            <ul class="social">
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-facebook" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-twitter" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-linkedin" aria-hidden="true"></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    @endforeach
+
+                                    <!-- <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                                        <div class="our-team">
+                                            <div class="picture">
+                                                <img class="img-fluid" src="https://picsum.photos/130/130?image=839">
+                                            </div>
+
+                                            <div class="team-content">
+                                                <h3 class="name">Patricia Knott</h3>
+                                                <h4 class="title">Web Developer</h4>
+                                            </div>
+
+                                            <ul class="social">
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-facebook" aria-hidden="true">
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-twitter" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-linkedin" aria-hidden="true"></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                                        <div class="our-team">
+                                            <div class="picture">
+                                                <img class="img-fluid" src="https://picsum.photos/130/130?image=856">
+                                            </div>
+
+                                            <div class="team-content">
+                                                <h3 class="name">Justin Ramos</h3>
+                                                <h4 class="title">Web Developer</h4>
+                                            </div>
+                                            <ul class="social">
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-facebook" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-twitter" aria-hidden="true"></a>
+                                                </li>
+                                                
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-linkedin" aria-hidden="true"></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                                        <div class="our-team">
+                                            <div class="picture">
+                                                <img class="img-fluid" src="https://picsum.photos/130/130?image=836">
+                                            </div>
+                                            
+                                            <div class="team-content">
+                                                <h3 class="name">Mary Huntley</h3>
+                                                <h4 class="title">Web Developer</h4>
+                                            </div>
+
+                                            <ul class="social">
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-facebook" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-twitter" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-linkedin" aria-hidden="true"></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div> -->
+
+                                </div>
+                            </div>
                         </div>
 
                         <!-- GRAPH VIEW -->
